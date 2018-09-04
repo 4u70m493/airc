fb8e1741f5fc290a2ecff7f4fa1141fa7ed75627
Valia Walx <walsk@yandex.ru>
2018-09-04 16:17:45 +0300

    adds redirection back to page where logged in

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from app.models import User
 
 # for forms
@@ -12,17 +13,24 @@ import calendar as cal
 
 
 # Define supported routes
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = EventParamsForm()
+    if form.validate_on_submit():
+        city = form.city.data
+        country = form.country.data
+        date_from = form.date_from.data
+        date_to = form.date_to.data
+
+        flash("Got: city, country, dates! Nice!")
     return render_template('index.html', title='Find best airshows around', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('my-events')) # TODO fix, if any
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -30,7 +38,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign in', form=form)
 
 @app.route('/logout')
@@ -38,11 +49,15 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@login_required
+@app.route('/my-events')
+def my_events():
+    pass # TODO do it actually
+
 @app.route('/calendar')
 def show_calendar():
     c = cal.HTMLCalendar(cal.MONDAY)
     return c.formatmonth(2018,9)
-
 
 @app.route('/hello')
 def hello_world():
